ebcc68417fa8b480c267b7fdda81a804a0a2a09f
Simon <simon.hablas@oeaw.ac.at>
2024-01-17 16:58:52 +0100

    Added user Info

--- Tools/python/user.py
+++ Tools/python/user.py
@@ -8,3 +8,12 @@ if os.environ["USER"] in ["dennis.schwarz"]:
     cache_dir                       = "/groups/hephy/cms/dennis.schwarz/MLUnfolding/caches"
     analysis_results                = "/groups/hephy/cms/dennis.schwarz/MLUnfolding/results/v1"
     cern_proxy_certificate          = "/users/dennis.schwarz/.private/.proxy"
+
+
+if os.environ["USER"] in ["simon.hablas"]:
+    postprocessing_output_directory = "/scratch-cbe/users/simon.hablas/MLUnfolding/nanoTuples"
+    postprocessing_tmp_directory    = "/scratch/hephy/cms/simon.hablas/MLUnfolding/tmp/"
+    plot_directory                  = "/groups/hephy/cms/simon.hablas/www/MLUnfolding/plots"
+    cache_dir                       = "/groups/hephy/cms/simon.hablas/MLUnfolding/caches"
+    analysis_results                = "/groups/hephy/cms/simon.hablas/MLUnfolding/results/v1"
+    cern_proxy_certificate          = "/users/simon.hablas/.private/.proxy"
